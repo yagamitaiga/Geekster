c79025ce236124e42006c0711993b8032ebd203e
yagamitaiga <104020890+yagamitaiga@users.noreply.github.com>
2022-04-21 00:29:28 +0530

    Add files via upload

---
+++ problem1.java
@@ -0,0 +1,18 @@
+package Day2;
+
+public class problem1 {
+	public static void main(String[] args) {
+		System.out.print("gaurav ");
+		System.out.print("is ");
+		System.out.print("invincible ");
+		String name = "and best";
+		System.out.print(name);
+		int age = 22;
+		String pre="At the age of";
+\\String s =Integer.toString(age);
+		System.out.print(pre + Integer.toString(age));
+		
+		
+	}
+
+}
